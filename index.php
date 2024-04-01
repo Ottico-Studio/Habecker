efe92caacacef76657e3805e889c7790c444f5b9
Ottico-Studio <ottico@mailfence.com>
2024-03-31 22:18:01 -0400

    First Up

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Matthew Habecker </title>
+
+    <link rel="stylesheet" href="assets/Main.css">
+
+</head>
+<body>
+    
+
+    <!-- NavBar -->
+    <nav>
+        <div>
+            <h1> Web<span>Dev</span> </h1>    
+        </div>
+        <div class="Menu">
+            <ul>
+                <li> Home </li>
+                <li> Services </li>
+                <li> About </li>
+                <li> Projects </li>
+                <li> Contact </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- END NavBar -->
+
+
+    <!-- HERO -->
+    <section class="Hero">
+
+        <div class="H-Left">
+            <div class="HL-Content">
+                <div class="Animated-Text">
+                    <h1> Hi I'm <span>Habby</span> </h1>
+                    <h2> Designer </h2>
+                </div>
+                <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Ad tempore, assumenda harum praesentium voluptate exercitationem 
+                    earum nesciunt temporibus. </p>
+
+                <div class="HL-Btns">
+                    <div class="Btn"> <p>Download CV</p> </div>
+                    <h3> Watch Video </h3>
+                </div>
+
+            </div>
+            
+        </div>
+
+        <div class="H-Right">
+            <img src="assets/imgs/Avatar.png" alt="">
+        </div>
+
+    </section>
+    <!-- END Hero -->
+
+
+
+
+</body>
+</html>
